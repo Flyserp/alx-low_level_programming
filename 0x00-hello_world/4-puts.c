212796a1e093f7dddfca01e3b06a1def25550c38
flyserp <valokoye9@gmail.com>
2022-04-07 10:53:53 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * 
- * Return: Always 0 (Success)
+ *  Main - Entry point
+ *
+ *  Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multiligual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
