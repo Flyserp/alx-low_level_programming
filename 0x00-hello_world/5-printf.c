53b017a0af5ebbc7cb39b54e6e33be2b12109ef9
flyserp <valokoye9@gmail.com>
2022-04-07 10:35:00 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
-#include <studio.h>
+#include <stdio.h>
+
 /**
- * Description: main - prints out to stdout.
- * Return: 0 if success.
- */
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-retun (0);
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
 }
